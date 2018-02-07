c3fb00cca7b1c41da042025ece278bf854ad7b1d
oscar arjona <oscar@DESKTOP-373J9G9>
2018-02-07 09:53:31 +0100

    final

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -1,7 +1,7 @@
 package com.desarrollos.entde;
 
-import java.util.Arrays;
-import java.awt.Color;
+//import java.util.Arrays;
+//import java.awt.Color;
 /**
  * Class that models objects of type "car"
  * 
